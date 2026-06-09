7cfd525e430005d68c5231a1e60550f4c7064b6a
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2026-06-09 10:36:04 +0200

    fix(tools): plan_update now stores and applies full updated content (#715)

--- assets/admin/index.asset.php
+++ assets/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a8dd5d0ad1d9f43da3c0');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dc70578c9371953055f3');
